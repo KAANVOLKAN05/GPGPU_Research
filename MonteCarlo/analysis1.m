461c1f4c73860ad230ca3fdf9dc2f3dd43b4b030
Kaan Volkan <kaan.volkan@cern.ch>
2026-07-28 19:43:33 -0400

    Update analysis1.m

--- MonteCarlo/analysis1.m
+++ MonteCarlo/analysis1.m
@@ -13,3 +13,17 @@ dim = dims = [60, 120, 60, 1];
 
 % Loading the fluence rate
 fluence_rate = loadmc2(filename, dims, "float");
+
+disp("Loaded array size:");
+disp(size(fluence_rate));
+
+
+% Coordinates of interest (detector position), +1 is because we are 0 indexed
+
+x_idx = x_mcx + 1;
+y_idx = y_mcx + 1;
+z_idx = z_mcx + 1;
+
+center_rate = fluence_rate(x_idx, y_idx, z_idx, 1);
+
+fprintf("Center fluence rate: %.8e\n", center_rate);
