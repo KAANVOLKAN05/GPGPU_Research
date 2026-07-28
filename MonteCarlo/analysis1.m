5bbcdb88680537389074a30e8166989f6e5bee96
Kaan Volkan <kaan.volkan@cern.ch>
2026-07-28 19:39:03 -0400

    Update analysis1.m

--- MonteCarlo/analysis1.m
+++ MonteCarlo/analysis1.m
@@ -1,9 +1,15 @@
 clear;
 clc;
 
-%This following path will take us to the mcx utils
-mcx_utils = "mcx/utils"
+% This following path will take us to the mcx utils
+mcx_utils = "mcx/utils";
 addpath(mcx_utils);
 
-%Name of the mcx output file
-filename = "KV_v1.mc2"
+% Name of the mcx output file
+filename = "KV_v1.mc2";
+
+% Dimentions of the geometry [Nx,Ny,Nz,num of time gates]
+dim = dims = [60, 120, 60, 1];
+
+% Loading the fluence rate
+fluence_rate = loadmc2(filename, dims, "float");
