90aa94a117a75341429b42df761abfdd2bc1b8ef
Kaan Volkan <kaan.volkan@cern.ch>
2026-08-12 11:48:52 -0400

    additions

--- MonteCarlo/MonteCarloProj/FornierForandExample2.m
+++ MonteCarlo/MonteCarloProj/FornierForandExample2.m
@@ -11,7 +11,15 @@ cfg.autopilot = 1; %good to keep 1
 load('TableGenerate/FournierForandTable.mat');
 
 %%%%%%%%%%%%%%%%% Variables %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-volume = ones(100, 100, 100);
+x_src_pos = 50;
+y_src_pos = 50;
+z_src_pos = 1;
+
+x_dim = 100;
+y_dim = 100;
+z_dim = 100;
+
+volume = ones(x_dim, y_dim, z_dim);
 rand_seed = randi([1 2^31-1],1,1);
 
 
@@ -23,7 +31,7 @@ cfg.prop = [0 0 1 1; 0.05 0.2 0 1.37];  % Defines the medium properties [mua mus
 %%%%%%%%%%%%%%%%% Source Parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Below are required
 cfg.nphoton = 1e6;
-cfg.srcpos = [30 30 1];
+cfg.srcpos = [x_src_pos y_src_pos z_src_pos];
 cfg.srcdir = [0,0,1];
 %cfg.srctype = 'pencil';
 
@@ -40,17 +48,30 @@ cfg.seed = rand_seed;
 
 % define phase function using cfg.invcdf
 cfg.invcdf = FFgenerator.invcdf;
-%flux = mcxlab(cfg);
 
 
-%%%%%%%%%%%%%%%%% Personal Plot Settings %%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%% apply mcxlab functions %%%%%%%%%%%%%%%%%%%%%%%%%
 
 fluxs = mcxlab(cfg);
 total_flux = sum(fluxs.data, 4);
+
+%%%%%%%%%%%%%%%%% Personal Plot Settings %%%%%%%%%%%%%%%%%%%%%%%%%
+
 figure;  %Opens a new figure window and makes it the active plotting window
-plotdata = squeeze(log(total_flux(:, 30, :)));
-imagesc(plotdata);
+%plotdata = squeeze(log(total_flux(:, 30, :)));
+%imagesc(plotdata);
 axis image; % Makes units on the x and y axis equally spaced
-colorbar; % Adds a color scale beside the image
-title('Template');
-print('Template.png', '-dpng', '-r300');
+%colorbar; % Adds a color scale beside the image
+%title('Template');
+%print('Template.png', '-dpng', '-r300');
+
+%%%%%%%%%%%%%%%%% Data Analysis %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+DataAnal = log(total_flux(:, 30, :));
+
+[idx, theta, I, R] = frontHemisphere2D(DataAnal, [30 30 1], [0 0 1], 20, 1);
+
+scatter(theta, I);
+xlabel('Angle (degrees)');
+ylabel('Fluence');
+grid on;
