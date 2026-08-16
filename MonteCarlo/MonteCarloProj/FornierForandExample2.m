8a70b7d84772d7383c77deea6eda89a91c407ad1
Kaan Volkan <kaan.volkan@cern.ch>
2026-08-15 20:54:52 -0400

    add

--- MonteCarlo/MonteCarloProj/FornierForandExample2.m
+++ MonteCarlo/MonteCarloProj/FornierForandExample2.m
@@ -77,7 +77,7 @@ hold on;
 for radius = radii
 
     [idx, theta, I, R] = frontHemisphere2D(total_flux, [50 50 1], [0 0 1], radius, 1);
-    logI = log(I);
+    logI = log10(I);
     scatter(theta, logI, 'DisplayName', sprintf('Radius = %d', radius));
 end
 
