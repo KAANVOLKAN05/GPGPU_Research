471ed5a369b5b13c9fe2162f9c8c766296838d83
Kaan Volkan <kaan.volkan@cern.ch>
2026-08-15 21:33:58 -0400

    additions

--- MonteCarlo/MonteCarloProj/FornierForandExample2.m
+++ MonteCarlo/MonteCarloProj/FornierForandExample2.m
@@ -76,7 +76,7 @@ hold on;
 
 for radius = radii
 
-    [idx, theta, I, R] = fullcircleanalysis(total_flux, [50 50 1], [0 0 1], radius, 1);
+    [idx, theta, I, R] = fullcircleanalysis(total_flux, [250 250 250], [0 0 1], radius, 1);
     logI = log10(I);
     scatter(theta, logI, 'DisplayName', sprintf('Radius = %d', radius));
 end
