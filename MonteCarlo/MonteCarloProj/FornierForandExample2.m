8da7e4daa2d8d786297626f660376fa9fc20fce1
Kaan Volkan <ka.volkan05@gmail.com>
2026-08-15 20:52:47 -0400

    additions

--- MonteCarlo/MonteCarloProj/FornierForandExample2.m
+++ MonteCarlo/MonteCarloProj/FornierForandExample2.m
@@ -76,7 +76,7 @@ hold on;
 
 for radius = radii
 
-    [idx, theta, I, R] = frontHemisphere2D(total_flux, [50 50 1], [0 0 1], radius, 1);
+    [idx, theta, I, R] = fullCircle2D(total_flux, [250 250 250],[0 0 1], radius, 1);
     logI = log(I);
     scatter(theta, logI, 'DisplayName', sprintf('Radius = %d', radius));
 end
