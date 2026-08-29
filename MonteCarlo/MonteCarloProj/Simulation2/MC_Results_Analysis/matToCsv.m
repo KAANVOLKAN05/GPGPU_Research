16ddb6769bdea7c524c29b2c2d6d93ace7ca507c
Kaan Volkan <ka.volkan05@gmail.com>
2026-08-29 19:29:11 -0400

    additional results

---
+++ MonteCarlo/MonteCarloProj/Simulation2/MC_Results_Analysis/matToCsv.m
@@ -0,0 +1,38 @@
+clear;
+clc;
+
+scriptDir = fileparts(mfilename('fullpath'));
+
+matFolder = fullfile(scriptDir, '..', 'MC_Results', 'data');
+outputFolder = fullfile(scriptDir, '..', 'MC_Results', 'runs');
+
+if ~exist(outputFolder, 'dir')
+    mkdir(outputFolder);
+end
+
+files = dir(fullfile(matFolder, 'run_*.mat'));
+
+fprintf('Found %d MAT files.\n', length(files));
+
+for i = 1:length(files)
+
+    data = load(fullfile(matFolder, files(i).name));
+
+    x = data.x_bin_positions(:);
+    y = data.x_bins(:);
+
+    [~, runID, ~] = fileparts(files(i).name);
+
+    csvFile = fullfile(outputFolder, [runID '.csv']);
+
+    fid = fopen(csvFile, 'w');
+    fprintf(fid, 'x_bin_position,x_bin\n');
+
+    for j = 1:length(x)
+        fprintf(fid, '%.10g,%.10g\n', x(j), y(j));
+    end
+
+    fclose(fid);
+end
+
+fprintf('Created %d CSV files in:\n%s\n', length(files), outputFolder);
